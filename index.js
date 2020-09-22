78da14e575a25bbcd0fb4fa05cac00a3ab622676
bilelrahmouni <bilelrahmouni01@hotmail.fr>
2020-09-22 17:32:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "home.html"));
+// });
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.listen(port, () => {
+  console.log(`Server fired at http://localhost:${port}`);
+});
