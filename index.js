06784dac21ebe22a3e3265cb2dae180a93ce5e6c
bilelrahmouni <bilelrahmouni01@hotmail.fr>
2020-09-22 22:26:50 +0100

    add time condition

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const moment = require("moment");
 const path = require("path");
 const app = express();
 const port = 3000;
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "home.html"));
 // });
-app.use("/static", express.static(path.join(__dirname, "public")));
+if (
+  moment().day() >= 1 &&
+  moment().hours() >= 9 &&
+  moment().day() <= 5 &&
+  moment().hours() <= 19
+) {
+  app.use(express.static("public"));
+} else {
+  app.get("*", (req, res) => {
+    res.status(400);
+    res.send(
+      "<h1>The web application is only available in working time (Monday to Friday,  from 9 to 17) </h1>"
+    );
+  });
+}
 app.listen(port, () => {
   console.log(`Server fired at http://localhost:${port}`);
 });
